f3cdb0a38e0b17c03d33a046d841463fbb737709
KavinduRankothge-tech <kavindunilhan.rankothge@students.mq.edu.au>
2021-09-10 16:36:11 +0530

    Initial commit with hotel model

---
+++ SpringMongoApp/src/main/java/com/demo/app/model/Hotel.java
@@ -0,0 +1,69 @@
+package com.demo.app.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.tomcat.jni.Address;
+
+public class Hotel {
+	private String id;
+	private String name;
+	private int pricePerNight;
+	private Address address;
+	private List<Review> reviews;
+
+
+	protected Hotel() {
+		this.reviews = new ArrayList<>();		
+	}
+
+	public Hotel(String name, int pricePerNight, Address address, List<Review> reviews) {
+		this.name = name;
+		this.pricePerNight = pricePerNight;
+		this.address = address;
+		this.reviews = reviews;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPricePerNight() {
+		return pricePerNight;
+	}
+
+	public void setPricePerNight(int pricePerNight) {
+		this.pricePerNight = pricePerNight;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}	
+}
